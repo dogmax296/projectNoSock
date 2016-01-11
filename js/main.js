0c0fa5bbe262ffbe1a59d4ad824ba5016a891b24
dogmax296 <ivanov1maxim@gmail.com>
2016-01-11 14:51:56 +0200

    added:
     - Product carousel;
     - Product info;

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.main-store__carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    },
+    navText : ['', '']
+
+})
